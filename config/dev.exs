370d9fcaa0348d683fcdc358ecb97feb659ef5f8
Raphael Telatim <raphatelatim@gmail.com>
2021-08-20 17:14:51 -0300

    add configs

---
+++ config/dev.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :skrappy, :http, client: HTTPoison
+
+config :skrappy, :http,
+  base_url:
+    "https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
---
+++ config/test.exs
@@ -0,0 +1,4 @@
+import Config
+
+config :skrappy, :http, client: Skrappy.Http.ClientMock
+config :skrappy, :http, base_url: "https://wowsuchurl.com"
