e0596f79133748eb08bd4596af4f1cdcda44c643
Raphael Telatim <raphatelatim@gmail.com>
2021-08-22 21:26:05 -0300

    add fraud detect function

--- lib/skrappy.ex
+++ lib/skrappy.ex
@@ -1,18 +1,24 @@
 defmodule Skrappy do
   @moduledoc """
-  Documentation for `Skrappy`.
+  Scrapes N (default 5) pages from www.dealerrater.com and
+  returns the 3 most likely fraudulent reviews based
+  on a pre determined list of suspicious words.
   """
 
-  @doc """
-  Hello world.
+  alias Skrappy.Entities.Review
+  alias Skrappy.Http.Client, as: HttpClient
+  alias Skrappy.Review.Adapter, as: ReviewAdapter
+  alias Skrappy.Review.Analyzer, as: ReviewAnalyzer
 
-  ## Examples
-
-      iex> Skrappy.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec fraud_detect(max_pages :: integer()) :: list(struct)
+  def fraud_detect(max_pages \\ 5) do
+    1..max_pages
+    |> Enum.map(&HttpClient.scrap_page/1)
+    |> Enum.reduce([], fn {status, html}, acc ->
+      if status == :ok, do: [html | acc]
+    end)
+    |> Enum.flat_map(&ReviewAdapter.from_html/1)
+    |> Enum.map(&Review.new/1)
+    |> ReviewAnalyzer.most_suspicious(3)
   end
 end
